811f92c800c3e372314618152c91656e69ffd2c1
김형석 <khkhvv@gmail.com>
2022-04-18 00:56:17 +0900

    fix(bug fix): countOfIncluding 두 리스트비교 로직

--- src/main/java/baseball/domain/GameRule.java
+++ src/main/java/baseball/domain/GameRule.java
@@ -1,8 +1,5 @@
 package baseball.domain;
 
-import camp.nextstep.edu.missionutils.Randoms;
-
-import java.util.ArrayList;
 import java.util.List;
 
 public class GameRule {
@@ -22,7 +19,7 @@ public class GameRule {
     public static int countOfIncluding(List<Integer> computerBall, List<Integer> userBall){
         int result = 0;
         for(int i : computerBall){
-            if(computerBall.contains(i)) {
+            if(userBall.contains(i)) {
                 result++;
             }
         }
