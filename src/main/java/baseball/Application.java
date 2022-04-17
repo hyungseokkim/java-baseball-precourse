8c97fc762ed1430dc6974f229df251e5af87679c
김형석 <khkhvv@gmail.com>
2022-04-18 01:00:54 +0900

    feat(main): main에서 게임 실행 로직 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,10 @@
 package baseball;
 
+import baseball.domain.Starter;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-
+        Starter starter = new Starter();
+        starter.run();
     }
 }
