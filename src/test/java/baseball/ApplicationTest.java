dfbc222d89ea0ecd371b6d67d07e7b74a3a5b896
김형석 <khkhvv@gmail.com>
2022-04-17 13:27:31 +0900

    Fix(applicationTest): default 테스트코드 한글메소드명 커밋 안되던 인텔리제이 설정 변경

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -10,7 +10,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
     @Test
-    void gameSetAndRestart() {
+    void 게임종료_후_재시작() {
         assertRandomNumberInRangeTest(
                 () -> {
                     run("246", "135", "1", "597", "589", "2");
@@ -21,7 +21,7 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void exceptionTest() {
+    void 예외_테스트() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("1234"))
                         .isInstanceOf(IllegalArgumentException.class)
@@ -32,5 +32,4 @@ class ApplicationTest extends NsTest {
     public void runMain() {
         Application.main(new String[]{});
     }
-
 }
